b4b066d85692b014988ce7d2c2845712c6ebe7af
seenso <>
2021-11-02 12:23:42 -0800

    Passed test for findMatching()

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution here
+let findMatching = (arr, str) => {
+  return arr.filter(arr => arr.toUpperCase() === str.toUpperCase());
+};
+
+let fuzzyMatch = () => {};
+
+let matchName = () => {};
