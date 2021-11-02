5a8c6c9dc6083e8ecba2c92ccab95d6cfa1174fd
seenso <>
2021-11-02 13:17:38 -0800

    Working on fuzzyMatch(). Test 1 is not passing.

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // Code your solution here
 let findMatching = (arr, str) => {
-  return arr.filter(arr => arr.toUpperCase() === str.toUpperCase());
+  return arr.filter(driver => driver.toUpperCase() === str.toUpperCase());
 };
 
-let fuzzyMatch = () => {};
+let fuzzyMatch = (arr, str) => {
+  return arr.filter(driver => {
+   driver.includes(str);
+  });
+};
 
-let matchName = () => {};
+let matchName = (arr, str) => {
+  return arr.filter(driver => {
+    driver.name === str;
+  });
+};
